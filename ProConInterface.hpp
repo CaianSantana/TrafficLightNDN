85b5bf4948a2fab5223ab25c483ff979d3f2a3a4
caian23 <caian23@outlook.com>
2025-06-03 01:00:36 -0300

    ADD: ProConInterface.hpp DELETE: ProConInterface.cpp

---
+++ ProConInterface.hpp
@@ -0,0 +1,47 @@
+#ifndef PROCONINTERFACE_HPP
+#define PROCONINTERFACE_HPP
+
+#include <ndn-cxx/face.hpp>
+#include <ndn-cxx/util/scheduler.hpp>
+#include <boost/asio/io_context.hpp>
+#include <ndn-cxx/security/key-chain.hpp>
+#include <ndn-cxx/security/signing-helpers.hpp>
+#include <ndn-cxx/security/validator-config.hpp>
+
+#include <iostream>
+#include <string>
+#include <random>
+#include <chrono> 
+#include <unistd.h>
+#include <typeinfo>
+
+namespace ndn {
+class ProConInterface {
+public:
+    virtual ~ProConInterface() = default;
+
+    virtual void setup(std::string prefix = "/prefixo/exemplo", int id) = 0;
+    virtual void run(const std::string& url) = 0;
+
+protected:
+    virtual void runProducer() = 0;
+
+    virtual void onData(const Interest&, const Data& data) = 0;
+
+    virtual void onNack(const Interest& interest, const lp::Nack& nack) = 0;
+
+    virtual void onTimeout(const Interest& interest) = 0;
+
+    virtual Interest createInterest(std::string url) = 0;
+
+    virtual void sendInterest(const Interest& interest) = 0;
+
+    virtual void runConsumer(std::string url) = 0;
+
+    virtual void onInterest(const Interest& interest) = 0;
+
+    virtual void onRegisterFailed(const Name& prefix, const std::string& reason) = 0;
+};
+} // namespace ndn
+
+#endif // PROCONINTERFACE_HPP
